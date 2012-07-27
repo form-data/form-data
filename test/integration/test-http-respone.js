383cdd473de6d8cb5180a90c14238b59115c4aca
Alex Indigo <iam@alexindigo.com>
2012-07-26 22:04:03 -0700

    Added support for https destination, http-response and mikeal's request streams.
    Use cases:
    
    - https destination: upload photos to api.500px.com
    
    - remote file streams (http-response, mikeal's request): Upload photos from Flickr to 500px.com or vice versa.

--- test/integration/test-http-respone.js
+++ test/integration/test-http-respone.js
@@ -4,26 +4,64 @@ var http = require('http');
 var path = require('path');
 var mime = require('mime');
 var request = require('request');
+var parseUrl = require('url').parse;
 var fs = require('fs');
 var FormData = require(common.dir.lib + '/form_data');
 var IncomingForm = require('formidable').IncomingForm;
 
 var remoteFile = 'http://nodejs.org/images/logo.png';
 
-var FIELDS = [
-  {name: 'my_field', value: 'my_value'},
-  {name: 'my_buffer', value: new Buffer([1, 2, 3])},
-  {name: 'my_file', value: fs.createReadStream(common.dir.fixture + '/unicycle.jpg')},
-  {name: 'remote_file', value: request(remoteFile) }
-];
+var FIELDS;
+var server;
 
-var server = http.createServer(function(req, res) {
+var parsedUrl = parseUrl(remoteFile)
+  , options =
+    {
+      method: 'get',
+      port: parsedUrl.port || 80,
+      path: parsedUrl.pathname,
+      host: parsedUrl.hostname
+    }
+  ;
+
+http.request(options, function(res)
+{
+
+  FIELDS = [
+    {name: 'my_field', value: 'my_value'},
+    {name: 'my_buffer', value: new Buffer([1, 2, 3])},
+    {name: 'remote_file', value: res }
+  ];
+
+  var form = new FormData();
+  FIELDS.forEach(function(field) {
+    form.append(field.name, field.value);
+  });
+
+  server.listen(common.port, function() {
+
+    form.submit('http://localhost:' + common.port + '/', function(err, res) {
+
+      if (err) {
+        throw err;
+      }
+
+      assert.strictEqual(res.statusCode, 200);
+      server.close();
+    });
+
+  });
+
+
+}).end();
+
+server = http.createServer(function(req, res) {
 
   // formidable is broken so let's do it manual way
   //
   // var form = new IncomingForm();
   // form.uploadDir = common.dir.tmp;
-  // form.parse(req);
+  //  form.parse(req);
   // form
   //   .on('field', function(name, value) {
   //     var field = FIELDS.shift();
@@ -34,6 +72,7 @@ var server = http.createServer(function(req, res) {
   //     var field = FIELDS.shift();
   //     assert.strictEqual(name, field.name);
   //     assert.strictEqual(file.name, path.basename(field.value.path));
+  //     // mime.lookup file.NAME == 'my_file' ?
   //     assert.strictEqual(file.type, mime.lookup(file.name));
   //   })
   //   .on('end', function() {
@@ -50,6 +89,7 @@ var server = http.createServer(function(req, res) {
   });
   req.on('end', function()
   {
+
     // check for the fields' traces
 
     // 1st field : my_field
@@ -62,18 +102,10 @@ var server = http.createServer(function(req, res) {
     assert.ok( data.indexOf('form-data; name="'+field.name+'"') != -1 );
     assert.ok( data.indexOf(field.value) != -1 );
 
-    // 3rd field : my_file
+    // 3rd field : remote_file
     var field = FIELDS.shift();
     assert.ok( data.indexOf('form-data; name="'+field.name+'"') != -1 );
-    assert.ok( data.indexOf('; filename="'+path.basename(field.value.path)+'"') != -1 );
-    // check for unicycle.jpg traces
-    assert.ok( data.indexOf('2005:06:21 01:44:12') != -1 );
-    assert.ok( data.indexOf('Content-Type: '+mime.lookup(field.value.path) ) != -1 );
-
-    // 4th field : remote_file
-    var field = FIELDS.shift();
-    assert.ok( data.indexOf('form-data; name="'+field.name+'"') != -1 );
-    assert.ok( data.indexOf('; filename="'+path.basename(field.value.path)+'"') != -1 );
+    assert.ok( data.indexOf('; filename="'+path.basename(remoteFile)+'"') != -1 );
     // check for http://nodejs.org/images/logo.png traces
     assert.ok( data.indexOf('ImageReady') != -1 );
     assert.ok( data.indexOf('Content-Type: '+mime.lookup(remoteFile) ) != -1 );
@@ -83,28 +115,8 @@ var server = http.createServer(function(req, res) {
 
   });
 
-
 });
 
-server.listen(common.port, function() {
-  var form = new FormData();
-  FIELDS.forEach(function(field) {
-    form.append(field.name, field.value);
-  });
-
-  var request = http.request({
-    method: 'post',
-    port: common.port,
-    path: '/upload',
-    headers: form.getHeaders()
-  });
-
-  form.pipe(request);
-
-  request.on('response', function(res) {
-    server.close();
-  });
-});
 
 process.on('exit', function() {
   assert.strictEqual(FIELDS.length, 0);
