837b8a1f7562bfb8bda74f3fc538adb7a5858995
Mallikarjun-0 <saimallikarjun9@gmail.com>
2025-04-16 03:56:06 +0530

    [Tests] add tests to check the behavior of `getBoundary` with non-strings

--- test/integration/test-set-boundary.js
+++ test/integration/test-set-boundary.js
@@ -23,3 +23,13 @@ var FormData = require(common.dir.lib + '/form_data');
   assert.equal(formA.getBoundary(), userBoundary);
   assert.notEqual(formA.getBoundary(), formB.getBoundary());
 }());
+
+(function testsetBoundaryWithNonString_preExistingBehavior() {
+  var form = new FormData();
+
+  var nonStringValues = [123, { value: 123 }, ['---something']];
+  nonStringValues.forEach(function (value) {
+    form.setBoundary(value);
+    assert.equal(form.getBoundary(), value);
+  });
+}());
