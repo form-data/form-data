0e93122358414942393d9c2dc434ae69e58be7c8
Jordan Harband <ljharb@gmail.com>
2025-07-16 19:34:29 -0700

    [Tests] handle predict-v8-randomness failures in node < 17 and node > 23
    See https://github.com/matthewoestreich/predict-v8-randomness/issues/2

--- test/integration/test-boundary-prediction.js
+++ test/integration/test-boundary-prediction.js
@@ -3,7 +3,8 @@
 var common = require('../common');
 var assert = common.assert;
 var FormData = require(common.dir.lib + '/form_data');
-var predictV8Randomness = require('predict-v8-randomness');
+var satisfies = require('semver').satisfies;
+var predictV8Randomness = satisfies(process.version, '^17 - ^23') && require('predict-v8-randomness'); // eslint-disable-line global-require
 
 var initialSequence = [
   Math.random(),
@@ -11,42 +12,44 @@ var initialSequence = [
   Math.random(),
   Math.random(),
 ];
-var predictor = new predictV8Randomness.Predictor(initialSequence);
+var predictor = predictV8Randomness && new predictV8Randomness.Predictor(initialSequence);
 
-predictor.predictNext(24).then(function (next24RandomOutputs) {
-  var predictedBoundary = next24RandomOutputs
-    .map(function (v) {
-      return Math.floor(v * 10).toString(16);
-    })
-    .join('');
+if (predictor) {
+  predictor.predictNext(24).then(function (next24RandomOutputs) {
+    var predictedBoundary = next24RandomOutputs
+      .map(function (v) {
+        return Math.floor(v * 10).toString(16);
+      })
+      .join('');
 
-  var boundaryIntro = '----------------------------';
+    var boundaryIntro = '----------------------------';
 
-  var payload = 'zzz\r\n' + boundaryIntro + predictedBoundary + '\r\nContent-Disposition: form-data; name="is_admin"\r\n\r\ntrue\r\n' + boundaryIntro + predictedBoundary + '--\r\n';
+    var payload = 'zzz\r\n' + boundaryIntro + predictedBoundary + '\r\nContent-Disposition: form-data; name="is_admin"\r\n\r\ntrue\r\n' + boundaryIntro + predictedBoundary + '--\r\n';
 
-  var FIELDS = {
-    my_field: {
-      value: payload,
-    },
-  };
+    var FIELDS = {
+      my_field: {
+        value: payload,
+      },
+    };
 
-  // count total
-  var fieldsPassed = Object.keys(FIELDS).length;
+    // count total
+    var fieldsPassed = Object.keys(FIELDS).length;
 
-  // prepare form-receiving http server
-  var server = common.testFields(FIELDS, function (fields) {
-    fieldsPassed = fields;
+    // prepare form-receiving http server
+    var server = common.testFields(FIELDS, function (fields) {
+      fieldsPassed = fields;
+    });
+
+    server.listen(common.port, function () {
+      var form = new FormData();
+
+      common.actions.populateFields(form, FIELDS);
+
+      common.actions.submit(form, server);
+    });
+
+    process.on('exit', function () {
+      assert.strictEqual(fieldsPassed, 0);
+    });
   });
-
-  server.listen(common.port, function () {
-    var form = new FormData();
-
-    common.actions.populateFields(form, FIELDS);
-
-    common.actions.submit(form, server);
-  });
-
-  process.on('exit', function () {
-    assert.strictEqual(fieldsPassed, 0);
-  });
-});
+}
